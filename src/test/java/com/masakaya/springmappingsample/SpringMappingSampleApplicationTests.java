8ce0a7479e9150e2b873487ab4d7245c757f13b7
sylphs.mb@gmail.com <nly4cll@>
2018-09-26 08:00:37 +0900

    add forget file.

---
+++ src/test/java/com/masakaya/springmappingsample/SpringMappingSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.masakaya.springmappingsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMappingSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
